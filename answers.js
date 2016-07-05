5331cc59c54c255d3292f0dbacb7b37bb43e7347
JessMitch <jessicamitch42@gmail.com>
2016-07-05 18:08:23 +0000

    functions exercise 2 :)

--- answers.js
+++ answers.js
@@ -7,7 +7,18 @@ function firstLetter(word) {
     return word.charAt(0);
 }
 
-console.log(firstLetter("Jessica"));
-console.log(firstLetter("hello!"));
-console.log(firstLetter(""));
+// console.log(firstLetter("Jessica"));
+// console.log(firstLetter("hello!"));
+// console.log(firstLetter(""));
 
+/*
+Write a function that takes a string and returns the last character of a string. Test your function on a few inputs, including the empty string.
+*/
+
+function lastLetter(word) {
+    return word[word.length - 1];
+}
+
+// console.log(lastLetter("Jessica"));
+// console.log(lastLetter("hello!"));
+// console.log(lastLetter(""));
